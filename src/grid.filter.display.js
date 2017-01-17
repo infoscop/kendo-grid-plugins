ab5ae50fb3864c9ec4e42423aad0e3dbe85a9442
Catalin Ignat <catalin.ignat@infoscop.ro>
2017-01-17 11:52:43 +0200

    bugfix filter display boolean value

--- src/grid.filter.display.js
+++ src/grid.filter.display.js
@@ -36,7 +36,7 @@
                                             }
                                         } else {
                                             $.each(val.filters, function (i, v) {
-                                                if (v && v.field == unescape(field) && isEqual(v.value, value)) {
+                                                if (v && v.field == unescape(field) && methods.isEqual(v.value, value)) {
                                                     val.filters.splice(i, 1);
                                                     if (val.filters.length == 1) {
                                                         filter.filters[index] = val.filters[0];
